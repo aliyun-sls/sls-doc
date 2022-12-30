8d6fa85313c65adaf33881d0e7508161477deeb9
常旗 <changjiangmingxj@163.com>
2022-12-30 10:59:36 +0800

    feat ： small modify

--- .vitepress/siderbars/getOscompatibleDemo.js
+++ .vitepress/siderbars/getOscompatibleDemo.js
@@ -10,7 +10,7 @@ function getSidebar() {
                 {text: 'Java 消费示例', link: '/oscompatibledemo/java_kafka_consume'},
                 {text: 'Confluent-Kafka-Go 消费示例', link: '/oscompatibledemo/confluent_go_kafka_consume'},
                 {text: 'Franz-Kafka-Go 消费示例', link: '/oscompatibledemo/franz_go_kafka_consume'},
-                {text: 'python-Kafka 消费示例', link: '/oscompatibledemo/python_kafka_consume'}],
+                {text: 'Python-Kafka 消费示例', link: '/oscompatibledemo/python_kafka_consume'}],
         }
 
     ]
