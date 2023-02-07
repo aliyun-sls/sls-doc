c2589e13d1bca1793c6b172ea095e0a2c010757b
灵亦 <yuecjn@gmail.com>
2023-02-07 14:11:00 +0800

    feat: disable rum res

--- .vitepress/theme/src/components/utils.ts
+++ .vitepress/theme/src/components/utils.ts
@@ -64,7 +64,7 @@ export function initRum() {
       env: 'prod',
       service: 'web',
       enableError: true,
-      enableResourcePerf: true,
+      enableResourcePerf: false,
       enableAjax: true,
       enablePerf: true,
       enableTrace: false,
