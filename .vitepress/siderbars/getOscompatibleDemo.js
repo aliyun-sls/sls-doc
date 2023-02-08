93f50c5cd29dc5e7c99c648ebce9b282cd36cd15
常旗 <changjiangmingxj@163.com>
2023-02-08 09:18:42 +0800

    feat ： sarama kafka consume case

--- .vitepress/siderbars/getOscompatibleDemo.js
+++ .vitepress/siderbars/getOscompatibleDemo.js
@@ -9,8 +9,10 @@ function getSidebar() {
             items: [{text: '通过 Kafka 协议消费数据功能说明', link: '/oscompatibledemo/overview'},
                 {text: 'Java 消费示例', link: '/oscompatibledemo/java_kafka_consume'},
                 {text: 'Confluent-Kafka-Go 消费示例', link: '/oscompatibledemo/confluent_go_kafka_consume'},
+                {text: 'Sarama-Kafka-Go 消费示例', link: '/oscompatibledemo/sarama_go_kafka_consume'},
                 {text: 'Franz-Kafka-Go 消费示例', link: '/oscompatibledemo/franz_go_kafka_consume'},
                 {text: 'Python-Kafka 消费示例', link: '/oscompatibledemo/python_kafka_consume'}],
+
         }
 
     ]
