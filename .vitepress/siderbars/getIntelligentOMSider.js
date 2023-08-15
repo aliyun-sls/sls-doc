cedc901d94f0808f584a2531dd4ca44039fcfe84
灵亦 <yuecjn@gmail.com>
2023-08-15 14:45:11 +0800

    feat: add 异常检测

--- .vitepress/siderbars/getIntelligentOMSider.js
+++ .vitepress/siderbars/getIntelligentOMSider.js
@@ -10,6 +10,7 @@ function getSidebar() {
         { text: '全栈监控快速介绍', link: '/intelligentom/fullmonitorintro.md' },
         { text: '全栈可观测快速介绍', link: '/intelligentom/fullstackintro.md' },
         { text: 'Trace 服务快速介绍', link: '/intelligentom/traceintro.md' },
+        { text: '智能异常分析介绍', link: '/intelligentom/anomalyanalysisintro.md' },
       ],
     },
   ]
