72cd39989e364bcdab97f3c3d71afa4a138f9746
ruiqi <ruiqi.hy@alibaba-inc.com>
2024-02-22 15:52:59 +0800

    init dashdoard doc

--- .vitepress/siderbars/getVisulizationSider.js
+++ .vitepress/siderbars/getVisulizationSider.js
@@ -4,13 +4,459 @@ function getSidebar() {
       text: '可视化案例',
       items: [{ text: '案例总览', link: '/visulization/index' }],
     },
+    // 文件路径 '/visulization/generalDashbaord/xxxx.md'
     {
-      text: '常见图表案例',
+      text: '仪表盘通用案例',
       items: [
-        { text: '如何添加多Y轴线图', link: '/visulization/doubley.md' },
-        { text: '如何配置一个带有迷你图的单值图', link: '/visulization/singleWithMiniChart.md' },
-        { text: '过滤器最佳实践', link: '/visulization/filter.md' },
-        { text: '导入仪表盘最佳实践', link: '/visulization/importOtherProjectDashboard.md' },
+        { text: '如何添加多Y轴线图', link: '/visulization/generalDashbaord/doubley.md' },
+        { text: '如何配置一个带有迷你图的单值图', link: '/visulization/generalDashbaord/singleWithMiniChart.md' },
+        { text: '过滤器最佳实践', link: '/visulization//generalDashbaordfilter.md' },
+        {
+          text: '导入仪表盘最佳实践',
+          link: '/visulization/generalDashbaord/importOtherProjectDashboard.md',
+        },
+        {
+          text: '时序模式的理解方式',
+          link: '#',
+        },
+        {
+          text: '什么是字段配置',
+          link: '#',
+        },
+        {
+          text: '如何配置格式化（以线图和表格为例）',
+          link: '#',
+        },
+        {
+          text: '如何配置单位 （表格有个特殊的要求）',
+          link: '#',
+        },
+        {
+          text: '如何定制图例点击的效果 （以线图为例）',
+          link: '#',
+        },
+        {
+          text: '如何自定义 Tooltip 显示内容 （以线图和散点图为例）',
+          link: '#',
+        },
+        {
+          text: '图例如何实现排序及其效果 （流图特殊说明可以实现 topk 的能力）',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/tablePro/xxxx.md'
+    {
+      text: '表格Pro案例',
+      items: [
+        {
+          text: '如何配置一个基础表格',
+          link: '#',
+        },
+        {
+          text: '如何配置多查询语句的表格',
+          link: '#',
+        },
+        {
+          text: '如何通过数据转换将多个表格 join 在同一个表格中',
+          link: '#',
+        },
+        {
+          text: '如何配置表格行高',
+          link: '#',
+        },
+        {
+          text: '如何设置表格默认的排序字段',
+          link: '#',
+        },
+        {
+          text: '如何设置表格单元格文本高亮',
+          link: '#',
+        },
+        {
+          text: '如何设置表格单元格背景高亮',
+          link: '#',
+        },
+        {
+          text: '如何设置表格单元格整行背景高亮',
+          link: '#',
+        },
+        {
+          text: '如何设置表格单元格进度条样式（普通、LCD、渐变）',
+          link: '#',
+        },
+        {
+          text: '如何在表格单元格中设置线图、面积图、柱状图',
+          link: '#',
+        },
+        {
+          text: '表格如何支持行列转换',
+          link: '#',
+        },
+        {
+          text: '如何控制表格列的宽度',
+          link: '#',
+        },
+        {
+          text: '如何设置表格单元格的搜索和过滤功能',
+          link: '#',
+        },
+        {
+          text: '如何使用表格的阈值功能',
+          link: '#',
+        },
+        {
+          text: '如何在表格中使用变量替换',
+          link: '#',
+        },
+        {
+          text: '如何在表格中使用值映射',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/linePro/xxxx.md'
+    {
+      text: '线图Pro案例',
+      items: [
+        {
+          text: '如何配置一个基本线图',
+          link: '#',
+        },
+        {
+          text: '如何开启时序模式线图',
+          link: '#',
+        },
+        {
+          text: '如何配置两条或更多条的线图',
+          link: '#',
+        },
+        {
+          text: '如何给不同的线设置显示名（别名）',
+          link: '#',
+        },
+        {
+          text: '如何为不同的线配置不同颜色',
+          link: '#',
+        },
+        {
+          text: 'x轴时间范围、数据时间范围与查询时间范围之间的关系',
+          link: '#',
+        },
+        {
+          text: '线图如何配置x轴格式化',
+          link: '#',
+        },
+        {
+          text: '线图如何开启/关闭x轴',
+          link: '#',
+        },
+        {
+          text: '线图如何开启/关闭y轴',
+          link: '#',
+        },
+        {
+          text: '线图如何配置y轴范围',
+          link: '#',
+        },
+        {
+          text: '线图如何开启堆叠模式',
+          link: '#',
+        },
+        {
+          text: '如何配置一组数据为线图，另一组数据为柱状图',
+          link: '#',
+        },
+        {
+          text: '线图如何配置阈值',
+          link: '#',
+        },
+        {
+          text: '如何开启数据补全及其效果',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/barPro/xxxx.md'
+    {
+      text: '柱状图Pro案例',
+      items: [
+        {
+          text: '如何配置一个基本的柱状图',
+          link: '#',
+        },
+        {
+          text: '如何配置多查询的柱状图',
+          link: '#',
+        },
+        {
+          text: '如何配置堆叠柱状图',
+          link: '#',
+        },
+        {
+          text: '柱状图外观有哪些配置项',
+          link: '#',
+        },
+        {
+          text: '柱状图标签如何配置在柱子内部显示',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/aggPro/xxxx.md'
+    {
+      text: '流图Pro案例',
+      items: [
+        {
+          text: '如何配置一个基本的流图（分类线图）',
+          link: '#',
+        },
+        {
+          text: '流图分类数据限制说明',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/statPro/xxxx.md'
+    {
+      text: '统计图Pro案例',
+      items: [
+        {
+          text: '如何配置一个基本的统计图（单值图）',
+          link: '#',
+        },
+        {
+          text: '如何配置多查询的统计图',
+          link: '#',
+        },
+        {
+          text: '统计图如何配置值和标题',
+          link: '#',
+        },
+        {
+          text: '统计图有哪些布局方式',
+          link: '#',
+        },
+        {
+          text: '统计图如何配置对比值',
+          link: '#',
+        },
+        {
+          text: '统计图如何配置阈值',
+          link: '#',
+        },
+        {
+          text: '统计图如何配置趋势图',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/burgePro/xxxx.md'
+    {
+      text: '计量图Pro案例',
+      items: [
+        {
+          text: '如何配置一个基本的计量图',
+          link: '#',
+        },
+        {
+          text: '计量图如何配置多查询',
+          link: '#',
+        },
+        {
+          text: '如何配置一个刻度盘',
+          link: '#',
+        },
+        {
+          text: '计量图如何配置阈值',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/pipePro/xxxx.md'
+    {
+      text: '饼图Pro案例',
+      items: [
+        {
+          text: '如何配置一个饼图',
+          link: '#',
+        },
+        {
+          text: '饼图刻度文本有哪些配置选项',
+          link: '#',
+        },
+        {
+          text: '如何配置一个环形图',
+          link: '#',
+        },
+        {
+          text: '饼图如何配置多查询',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/histogram/xxxx.md'
+    {
+      text: '直方图Pro案例',
+      items: [
+        {
+          text: '如何配置一个直方图',
+          link: '#',
+        },
+        {
+          text: '如何限制直方图区间的范围和数量',
+          link: '#',
+        },
+        {
+          text: '如何配置堆叠直方图',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/radar/xxxx.md'
+    {
+      text: '雷达图Pro案例',
+      items: [
+        {
+          text: '如何配置一个基本的雷达图',
+          link: '#',
+        },
+        {
+          text: '雷达图的重要配置项有哪些',
+          link: '#',
+        },
+        {
+          text: '如何配置查询驱动的雷达图',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/crossTable/xxxx.md'
+    {
+      text: '交叉表Pro案例',
+      items: [
+        {
+          text: '如何配置一个交叉表Pro',
+          link: '#',
+        },
+        {
+          text: '如何配置多聚合字段的交叉表',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/scatter/xxxx.md'
+    {
+      text: '散点图Pro案例',
+      items: [
+        {
+          text: '如何配置一个散点图',
+          link: '#',
+        },
+        {
+          text: '散点图如何支持分类',
+          link: '#',
+        },
+        {
+          text: '如何动态设置散点图中点的大小',
+          link: '#',
+        },
+        {
+          text: '散点图如何设置阈值',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/topology/xxxx.md'
+    {
+      text: '拓扑图Pro案例',
+      items: [
+        {
+          text: '如何配置一个拓扑图',
+          link: '#',
+        },
+        {
+          text: '拓扑图如何设置连线指标',
+          link: '#',
+        },
+        {
+          text: '拓扑图如何配置节点类型',
+          link: '#',
+        },
+        {
+          text: '拓扑图如何配置图标类型节点',
+          link: '#',
+        },
+        {
+          text: '拓扑图支持哪些布局方式',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/markdownPro/xxxx.md'
+    {
+      text: 'MarkdownPro案例',
+      items: [
+        {
+          text: '如何配置 markdown 图表',
+          link: '#',
+        },
+        {
+          text: '如何在 markdown 图表中使用查询结果',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/mapPro/xxxx.md'
+    {
+      text: '地图Pro案例',
+      items: [
+        {
+          text: '如何配置中国地图',
+          link: '#',
+        },
+        {
+          text: '如何配置世界地图',
+          link: '#',
+        },
+        {
+          text: '如何配置高德地图',
+          link: '#',
+        },
+        {
+          text: '如何配置热力图',
+          link: '#',
+        },
+      ],
+    },
+    // 文件路径 '/visulization/drilldown/xxxx.md'
+    {
+      text: '交互事件案例',
+      items: [
+        {
+          text: '图表如何配置交互事件打开日志库',
+          link: '#',
+        },
+        {
+          text: '图表如何配置交互事件打开仪表盘',
+          link: '#',
+        },
+        {
+          text: '交互事件打开仪表盘如何设置新的变量',
+          link: '#',
+        },
+        {
+          text: '如何在交互事件中控制打开日志库的查询时间',
+          link: '#',
+        },
+        {
+          text: '如何在交互事件中打开 trace 详情',
+          link: '#',
+        },
+        {
+          text: '如何在交互事件中再次打开 trace 详情',
+          link: '#',
+        },
       ],
     },
   ]
---
+++ src/visulization/generalDashbaord/importOtherProjectDashboard.md
@@ -0,0 +1,108 @@
+# 导入仪表盘最佳实践
+
+使用导入仪表盘功能，您可以轻松地复制仪表盘，省去重新构建和配置的时间和精力。同时，该功能确保仪表盘的一致性和准确性，提高数据分析的效率和准确性。导入功能支持同 project导入、跨 project导入 和 修改 logstore。
+本文将通过具体场景为您介绍如何使用导入功能来复制仪表盘。
+
+## 前提条件
+
+已创建源仪表盘。具体操作，请参见[创建仪表盘](https://help.aliyun.com/document_detail/59324.htm#concept-osm-1nq-zdb)。
+
+## 导入配置
+
+通过配置相应的配置项，可以实现一键导入仪表盘。
+![image.png](/img/src/visulization/importOtherProjectDashboard/4a3d7d153429472af412228829099a49a6deb091ecfc3d585d2770e3cd7be629.png)
+
+**配置项说明：**
+
+| 参数              | 说明                                                                                                                       |
+| ----------------- | -------------------------------------------------------------------------------------------------------------------------- |
+| **源 Project**    | 选择源仪表盘所在的 Project。                                                                                               |
+| **源仪表盘**      | 选择待导入的源仪表盘。                                                                                                     |
+| **仪表盘名称**    | 设置新仪表盘的名称。                                                                                                       |
+| **Logstore 替换** | 如果新仪表盘需要依赖当前 Project 中的 Logstore，则您可以将源仪表盘所依赖的 Logstore 替换为当前 Project 下的目标 Logstore。 |
+
+## 使用场景一：导入相同 project 下的仪表盘
+
+源仪表盘：**simulator-nginx-demo** > **线图-显示柱状** <br />
+源 logstore：**nginx-access-log**
+![image.png](/img/src/visulization/importOtherProjectDashboard/fe1ed7808d138a0a1ea75534800750be4d1961e84493f9d4d01dae366dd1e9c1.png)
+目标仪表盘：**simulator-nginx-demo** > **线图-显示柱状-copy** <br />
+目标 logstore：**nginx-access-log**
+
+**操作步骤：**
+
+1. 登录[日志服务控制台](https://sls.console.aliyun.com/)。
+2. 在 Project 列表区域，单击目标 Project。
+3. 在左侧导航栏中，选择**仪表盘 > 仪表盘列表**。
+4. 在**仪表盘**区域右侧，选择 **+ > 导入仪表盘**。
+5. 配置相关参数<br />
+
+- 源 project：**simulator-nginx-demo**<br />
+- 源仪表盘：**线图-显示柱状**<br />
+- 仪表盘名称：**线图-显示柱状 copy**<br />
+
+![image.png](/img/src/visulization/importOtherProjectDashboard/7acea442d8c63f8be3361164e9d051f9fd5f9336dc462fe60db27e00b230eb8b.png)
+
+6. 点击导入，导入成功后，系统将跳转至仪表盘页面。
+
+![image.png](/img/src/visulization/importOtherProjectDashboard/011c188b8974e82478f28d8a144b5ab22d7d424f2c6c8bd349750810fbcd0139.png)
+
+## 使用场景二：修改 logstore
+
+源仪表盘：**simulator-nginx-demo** > **线图-显示柱状**<br />
+源 logstore：**nginx-access-log**<br />
+![image.png](/img/src/visulization/importOtherProjectDashboard/fe1ed7808d138a0a1ea75534800750be4d1961e84493f9d4d01dae366dd1e9c1.png)
+目标仪表盘：**simulator-nginx-demo** > **线图-显示柱状-copy** <br />
+目标 logstore：**test** <br />
+
+**操作步骤：**
+
+1. 登录[日志服务控制台](https://sls.console.aliyun.com/)。
+2. 在 Project 列表区域，单击目标 Project。
+3. 在左侧导航栏中，选择 **仪表盘 > 仪表盘列表**。
+4. 在**仪表盘**区域右侧，选择 **+ > 导入仪表盘**。
+5. 配置相关参数
+
+- 源 project：**simulator-nginx-demo** <br />
+- 源仪表盘：**线图-显示柱状** <br />
+- 仪表盘名称：**线图-显示柱状 copy** <br />
+- logstore 替换：**nginx-access-log** 替换为 **test** <br />
+  ![image.png](/img/src/visulization/importOtherProjectDashboard/c191cd33bea63a12a0a5f38ad7a496ad56d6d17dd617419d9ddbbd44557a9870.png)
+
+6. 点击导入，导入成功后，系统将跳转至仪表盘页面。
+
+![image.png](/img/src/visulization/importOtherProjectDashboard/011c188b8974e82478f28d8a144b5ab22d7d424f2c6c8bd349750810fbcd0139.png)
+
+7. 编辑图表,查看 logstore 被修改成了目标 logstore: **test**。
+
+![image.png](/img/src/visulization/importOtherProjectDashboard/a6468cb604d4408a25b0207794e328491bd469eca47266a459d06a838a23e627.png)
+
+## 使用场景三：导入不同 project 下的仪表盘
+
+源仪表盘：**simulator-nginx-demo** > **线图-显示柱状** <br />
+源 logstore：**nginx-access-log**<br />
+![image.png](/img/src/visulization/importOtherProjectDashboard/fe1ed7808d138a0a1ea75534800750be4d1961e84493f9d4d01dae366dd1e9c1.png)
+目标 Project：**wt-copy-nginx**<br />
+源 logstore：**wt-nginx-access-log**<br />
+
+**操作步骤：**
+
+1. 登录[日志服务控制台](https://sls.console.aliyun.com/)。
+2. 在 Project 列表区域，单击目标 Project。
+3. 在左侧导航栏中，选择**仪表盘 > 仪表盘列表**。
+4. 在**仪表盘**区域右侧，选择 **+ > 导入仪表盘**。
+5. 配置相关参数
+
+- 源 project：**simulator-nginx-demo**<br />
+- 源仪表盘：**线图-显示柱状**<br />
+- 仪表盘名称：**线图-显示柱状 copy**<br />
+- logstore 替换：**nginx-access-log** 替换为 **wt-nginx-access-log**
+  ![image.png](/img/src/visulization/importOtherProjectDashboard/9efea9a74efd81308ede28539a1712a8c21807367abb16ed16dce31257869af3.png)
+
+6. 点击导入，导入成功后，系统将跳转至仪表盘页面。
+
+![image.png](/img/src/visulization/importOtherProjectDashboard/e4c39a1d3909f3d75c6e88abc0d7c8384310bbcc6e47a4609342e47bb84cb91b.png)
+
+7. 编辑图表,查看 logstore 被修改成了目标 logstore: **wt-nginx-access-log**。
+
+![image.png](/img/src/visulization/importOtherProjectDashboard/9402b51fc48c3ef9c203432bb762c9eeb23d6aa5201b381acb9cbd4e65512d2c.png)
