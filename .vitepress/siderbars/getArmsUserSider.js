194be81a1570869114b92f010d4ecb92281f1cd0
xcaspar <changjun.xcj@alibaba-inc.com>
2024-09-18 16:26:24 +0800

    update user sider for new docs
    Signed-off-by: xcaspar <changjun.xcj@alibaba-inc.com>

--- .vitepress/siderbars/getArmsUserSider.js
+++ .vitepress/siderbars/getArmsUserSider.js
@@ -4,11 +4,11 @@ function getSidebar() {
       text: 'ARMS 用户体验监控',
       items: [
         { text: '案例总览', link: '/arms/user/home.md' },
-        { text: '追踪用户会话操作轨迹', link: '/arms/user/operationTrajectory.md' },
-        { text: '分析页面资源加载性能', link: '/arms/user/analysisPerformance.md' },
-        { text: '查询客户端资源加载性能', link: '/arms/user/searchPerformance.md' },
-        { text: '追踪用户会话', link: '/arms/user/trackUsers.md' },
-        { text: '分析页面功能访问量', link: '/arms/user/Analysisfunction.md' },
+        { text: '定位某用户产品使用报错', link: '/arms/user/locateUsageException.md' },
+        { text: '定位 App 页面打开缓慢', link: '/arms/user/locateAppPagePerformance.md' },
+        { text: '快速统计因故障造成用户影响面', link: '/arms/user/analyzeImpect.md' },
+        { text: '统计活动点击次数', link: '/arms/user/countingClick.md' },
+        { text: '快速定位白屏还原用户操作路径', link: '/arms/user/analyzePageBlank.md' },
       ],
     },
   ]
